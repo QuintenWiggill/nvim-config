c08009aa0720691ab604310af84d144cda2a3aa3
Quinten Wiggill <qwiggill@gmail.com>
2023-02-28 15:18:15 +0200

    Update nvim-tree startup behaviour

--- after/plugin/nvim-tree.rc.lua
+++ after/plugin/nvim-tree.rc.lua
@@ -13,9 +13,21 @@ require("nvim-tree").setup({
   },
 })
 
-local function open_nvim_tree()
+local function open_nvim_tree(data)
+  -- buffer is a real file on the disk
+  local real_file = vim.fn.filereadable(data.file) == 1
+  -- buffer is a [No Name]
+  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+  local directory = vim.fn.isdirectory(data.file) == 1
+  local api = require("nvim-tree.api")
 
-  require("nvim-tree.api").tree.open()
+  if real_file or no_name then
+    api.tree.open({ focus = false, find_file = true, })
+  elseif directory then
+    api.tree.open()
+  else
+    return
+  end
 end
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
