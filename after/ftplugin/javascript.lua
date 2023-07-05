d9618b76d0a12d9ea5c08a62e50d639e63734b71
Quinten Wiggill <81110438+QuintenWiggill@users.noreply.github.com>
2023-07-05 20:05:40 +0200

    Update ft indenting for macos

--- after/ftplugin/javascript.lua
+++ after/ftplugin/javascript.lua
@@ -1,2 +1,9 @@
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
+local has = vim.fn.has
+local is_mac = has "macunix"
+if is_mac then
+  vim.opt.shiftwidth = 4
+  vim.opt.tabstop = 4
+else
+  vim.opt.shiftwidth = 4
+  vim.opt.tabstop = 4
+end
